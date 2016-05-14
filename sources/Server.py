fccb220eb49500eff8dafd74a9a0fbf473c80cfc
Paul Dautry <paul.dautry@gmail.com>
2016-05-14 14:20:50 +0200

    Implementation des validateurs à l'inscription

--- sources/Server.py
+++ sources/Server.py
@@ -10,13 +10,14 @@ from flask import abort
 from flask import render_template
 from flask import flash
 from flask import jsonify
+from flask import espace
 from sources.utils import db
-from sources.utils import response
+from sources.utils.response import Response
 from sources.utils import validator
+from datetime import date
 
 # configuration
 DEBUG = True
-SECRET_KEY = 'development key'
 USERNAME = 'admin'
 PASSWORD = 'default'
 
@@ -25,7 +26,7 @@ PASSWORD = 'default'
 app = Flask(__name__)
 # Check Configuration section for more details
 app.config.from_object(__name__)
-app.secret_key = 'development_'
+app.secret_key = 'development_key'
 
 # --------------------------
 #   Fonction utilitaires
@@ -81,15 +82,24 @@ def signup():
     err = True
     content = "Une erreur s'est produite, l'inscription de l'utilisateur est annulée."
     if request.method == 'POST':
-        firstname = request.form['firstname']
-        lastname = request.form['lastname']
-        email = request.form['email'] 
+        firstname = escape(request.form['firstname'])
+        lastname = escape(request.form['lastname'])
+        email = request.form['email']
         pwd = request.form['password'] 
         promo = request.form['promo']
-        db.create_user(firstname, lastname, email, pwd, promo)
-        load_user(session, email, pwd)
-        err = False
-        content = 'ok'
+        # verification des champs
+        if not validator.validate(email, 'email'):
+            content = "L'email ne respecte pas le format attendu !"
+        elif not validator.validate(promo, 'year') and int(promo) <= date.today().year:
+            content = "La promo n'est pas une année correctement formaté"
+        else:
+            # creation de l'utilisateur
+            db.create_user(firstname, lastname, email, pwd, promo)
+            # chargement de l'utilisateur créé dans la session (connexion automatique après inscription)
+            load_user(session, email, pwd)
+            # mise à jour des variables de réponse 
+            err = False
+            content = 'ok'
     return jsonify(response=Response(err, content).json())
     
 
