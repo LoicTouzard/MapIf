09d1d6e661d1288a7ad69779e194062064775ebb
Loïc Touzard <loic.touzard@gmail.com>
2016-05-23 02:21:16 +0200

    Set minZoom to 2 + updated favicon close #2

--- src/static/js/main.js
+++ src/static/js/main.js
@@ -489,6 +489,7 @@ $(function(){
 	L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
 		attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
 		maxZoom: 18,
+		minZoom: 2,
 		id: 'ltouzard.0390bjno',
 		accessToken: SETTINGS.MAPBOXTOKEN
 	}).addTo(mymap);
