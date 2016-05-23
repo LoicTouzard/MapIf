bc13ef010ee42dc64fa425d17f5da1936462cf40
Paul Dautry <paul.dautry@gmail.com>
2016-05-23 21:52:20 +0200

    Add mapif.ini template. Add *.dist to gitignore. Update deployment script for more user friendly command line interaction (colors).

--- deployment/deploy_openshift.sh
+++ deployment/deploy_openshift.sh
@@ -1,21 +1,22 @@
 INI_DIR=app-root/runtime/repo/
 INI_FILE=mapif.ini
 APP=mapif
+BRANCH=prod
 
-becho() { echo -e "\033[34m[deploy_openshift]> ${1}\033[0m"; }
+colecho() { echo -e "\033[${1}m[deploy_openshift]> ${2}\033[0m"; }
+becho() { colecho 34 "${1}"; }
+gecho() { colecho 32 "${1}"; }
+recho() { colecho 31 "${1}"; }
+ok() { gecho "done."; }
+ko() { recho "failed!"; }
 
 becho "calling git to push latest commit to openshift..."
-git push openshift master
-becho "done!"
+(git push openshift ${BRANCH} && ok) ||ko
 becho "calling rhc to deploy app..."
-sudo rhc deploy master -a ${APP}
-becho "done!"
+(sudo rhc deploy ${BRANCH} -a ${APP} && ok) ||ko
 becho "calling rhc to send configuration file to openshift..."
-sudo rhc scp ${APP} upload ${INI_FILE} ${INI_DIR}
-becho "done!"
+(sudo rhc scp ${APP} upload ${INI_FILE} ${INI_DIR} && ok) ||ko
 becho "calling rhc to restart application..."
-sudo rhc app restart -a ${APP}
-becho "done!"
+(sudo rhc app restart -a ${APP} && ok) ||ko
 becho "calling rhc to read logs after deployement..."
-sudo rhc tail -a ${APP}
-becho "done!"
+(sudo rhc tail -a ${APP} && ok) ||ko
