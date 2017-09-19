f1ad63de9c6135f479d722a87c2a0f7bc1b6166e
Paul Dautry <paul.dautry@gmail.com>
2017-09-19 10:36:15 +0200

    Remove useless elements since Openshift will not be used from now on.

--- maintenance.py
+++ maintenance.py
@@ -1,25 +1,59 @@
+#!/usr/bin/env python3
+# -!- encoding:utf8 -!-
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#    file: maintenance.py
+#    date: 2017-09-19
+#  author: paul.dautry
+# purpose:
+#       Perform maintenance actions on DB
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#===============================================================================
+# IMPORTS 
+#===============================================================================
 from src.utils import db
 from src.utils import ini
-
-# ask for confirmation
-print("Do you really want to run this script ? [y/n]: ", end='')
-resp=input("");
-if resp != 'y':
-    exit()
-
-
-# load ini file
-_APP_ROOT_=ini.getenv('OPENSHIFT_REPO_DIR', '')
-if not ini.init_config(_APP_ROOT_+'mapif.ini'):
-    logger.mprint("Configuration file is missing. Server can't be started !")
-    exit(-1)
-# initialize database
-db.init_db()
-
-## -------------------------- PATCH DB V1.3.0 ------------------------------- ##
-## fix issue #14: safe password storage with salt and blowfish encryption
-#for u in db.get_all_users():
-#    print("Updating: ", u, "...", end='')
-#    db.update_user_password(u.id)
-#    print("done!")
-## -------------------------- PATCH DB V1.3.0 ------------------------------- ##
+#===============================================================================
+# FUNCTIONS 
+#===============================================================================
+#-------------------------------------------------------------------------------
+# fix_issue_14
+#       fix issue #14: safe password storage with salt and blowfish encryption
+#       PATCH DB V1.3.0
+#-------------------------------------------------------------------------------
+#def fix_issue_14():
+#    for u in db.get_all_users():
+#        print("Updating: ", u, "...", end='')
+#        db.update_user_password(u.id)
+#        print("done!")
+#-------------------------------------------------------------------------------
+# confirm
+#       ask for confirmation before running script
+#-------------------------------------------------------------------------------
+def confirm():
+    print("Do you really want to run this script ? [yes/n]: ", end='')
+    resp=input("");
+    if resp != 'yes':
+        exit(1)
+#-------------------------------------------------------------------------------
+# configure
+#-------------------------------------------------------------------------------
+def configure():
+    # load ini file
+    if not ini.init_config('mapif.ini'):
+        logger.mprint("Configuration file is missing. Server can't be started !")
+        exit(1)    
+    # initialize database
+    db.init_db()
+#-------------------------------------------------------------------------------
+# 
+#-------------------------------------------------------------------------------
+def main():
+    confirm()
+    configure()
+    #fix_issue_14()
+    exit(0)
+#===============================================================================
+# SCRIPT
+#===============================================================================
+if __name__ == '__main__':
+    main()
