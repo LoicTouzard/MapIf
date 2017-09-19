3de55e6793a9f95f7968392d7b8266f29d41e990
Paul Dautry <paul.dautry@gmail.com>
2017-09-19 11:50:16 +0200

    Add wsgi script.

---
+++ mapif_wsgi.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -!- encoding:utf8 -!-
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#    file: mapif_wsgi.py
+#    date: 2017-09-19
+#  author: paul.dautry
+# purpose:
+#   WSGI script for Nginx
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+from src.mapif import app
